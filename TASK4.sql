9af120d81c31f386de342b0e6e5ec58e43f9133e
Vamsi Pochampally <165893333+Vamsirusheel01@users.noreply.github.com>
2026-07-11 14:14:14 +0530

    Create college table and insert initial records
    Created a table for college students with initial data.

---
+++ TASK4.sql
@@ -0,0 +1,13 @@
+CREATE TABLE college (
+    student_id BIGINT,
+    student_name VARCHAR(50),
+    course VARCHAR(30),
+    fees FLOAT);
+    
+    
+INSERT INTO college (student_id, student_name, course, fees) VALUES 
+(1, 'Amit Sharma', 'Computer Science', 75000.00),
+(2, 'Priya Patel', 'Data Science', 82000.50),
+(3, 'John Doe', 'Information Technology', 70000.00),
+(4, 'Saniya Khan', 'Cyber Security', 85000.00),
+(5, 'Rahul Verma', 'Artificial Intelligence', 90000.00);
