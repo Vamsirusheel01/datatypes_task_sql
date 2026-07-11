961d953c0ab242dc672409285809fd544dbcf3b8
Vamsi Pochampally <165893333+Vamsirusheel01@users.noreply.github.com>
2026-07-11 14:04:02 +0530

    Create employee table and insert initial data

---
+++ TASK2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE employee (emp_id INT, emp_name VARCHAR(50), salary DECIMAL(10,2), department VARCHAR(30));
+INSERT INTO employee VALUES(001, "vamsi", 43000, 'CSE'), (002, "Arjun", 28000, 'ECE'),
+ (003, "Arjun", 43000, 'CSE'), (004, "Kavya", 41000, 'AI/ML'), (001, "Priya", 43000, 'CS');
+ SELECT * FROM employee;
+
